5d7c0fa24fb2b7f27643717544f88bbff2622aaa
Deepraj Adhikary <83914506+DeeprajDeveloper@users.noreply.github.com>
2026-07-08 19:31:12 -0400

    Delete data directory

--- data/index.js
+++
@@ -1,5 +0,0 @@
-export { site, navLinks, footer } from './site';
-export { hero } from './hero';
-export { careerJourney } from './career';
-export { caseStudies, featuredCaseStudies } from './case-studies';
-export { pages } from './pages';
